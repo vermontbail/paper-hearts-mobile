a58d5ea138cfdaf4be51fc004d52fa1407befae0
John J Hayes <34247425+ChefKef@users.noreply.github.com>
2022-01-30 16:03:10 -0500

    Create and link basic menus
    Made main menu and level select, linked scenes in a usable manner

--- Paper Hearts/Assets/Scripts/John/ToNewScene.cs
+++ Paper Hearts/Assets/Scripts/John/ToNewScene.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToNewScene : MonoBehaviour
 {
-
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
